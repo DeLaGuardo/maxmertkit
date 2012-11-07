4e15f9dde62b4d30373efcf9045af7b82cb025e5
Vetrenko Maxim <me@maxmert.com>
2012-11-07 20:34:34 +0400

    Began css animations

--- js/plugins/maxmertkit.js
+++ js/plugins/maxmertkit.js
@@ -1,242 +1,114 @@
 ;(function ( $, window, document, undefined ) {
 	
-	$.kit = function( name_, element_, options_ ) {
-		this.name = name_;
-		this.init();
+	var _name = 'kit'
+	,	_defaults = {
+			enabled: true
+		,	onlyOne: true
+		,	animation: null
+		,	animationDuration: 0
+		,	template: '<div>'
+		,	content: null
+		,	theme: 'dark'
+		,	trigger: 'hover'
+		,	delay: 0
+		}
+
+	$.kit = function(element, options) {
+		this.name = _name;
+		this.state = 'closed'; // closed in opened out
+		
+		this.element = element;
+		this.options = $.extend({}, _defaults, options);
+		this.El = $(this.options.template);
+
+		// this._setOptions( options );
 	}
 
-	$.kit.prototype.init = function() {
-		// console.log( 'init' );
+	$.kit.prototype._setOptions = function( options_ ) {
+		var me  = this;
+		var $me = $(me.element);
+
+		$.each( options_, function( key_, value_ ) {
+			me._setOption( key_, value_ );
+
+			var currentOption = { };
+				currentOption[ key_ ] = value_;
+
+			if( $.isFunction( value_ ) ) {
+				me._on( $me, currentOption );
+			}
+
+		});
+	}
+
+	$.kit.prototype._setOption = function( key_, value_ ) {
+		var me  = this;
+		var $me = $(me.element);
+
+		switch( key_ ) {
+			case 'theme':
+				me.El.removeClass( '-' + me.options.theme + '-' );
+				me.El.addClass( '-' + value_ + '-' )
+			break;
+
+			case 'enabled':
+				value_ === true ? me.El.removeClass( '-disabled-' ) : me.El.addClass( '-disabled-' );
+			break;
+		}
+
+		me.options[ key_ ] = value_;
+	}
+
+	$.kit.prototype._on = function( element_, handlers_ ) {
+		var me = this;
+
+		$.each( handlers_, function( event, handler ) {
+			element_.bind( event + '.' + me.name, handler );
+		});
+	}
+	
+	$.kit.prototype.enable = function() {
+		var me = this;
+
+		me._setOption( 'enabled', true )
+	}
+
+	$.kit.prototype.disable = function() {
+		var me = this;
+
+		me._setOption( 'enabled', false )
+	}
+
+	$.kit.prototype.getState = function() {
+		return this.state;
+	}
+
+	$.kit.prototype._getOtherInstanses = function( instanses_ ) {
+		var me = this;
+		
+		return $.grep( instanses_ , function( el ) {
+			return $.data($(el[0]), 'kit-' + me.name) !== me;
+		});
+		
+	}
+
+	$.kit.prototype._removeInstanse = function( instanses_ ) {
+		var me = this;
+		
+		var what
+		,	a = arguments.splice(0,1)
+		,	L = a.length
+		,	ax;
+
+		while( L && instanses_.length ) {
+			what = a[ --L ];
+			
+			while( (ax = instanses_.indexOf( what ) ) != -1 ){
+				instanses_.splice( ax, 1 );
+			}
+		}
+
+		return me;
 	}
 
 })( jQuery, window, document );
-
-// ;(function ( $, window, document, undefined ) {
-
-// 	slice = Array.prototype.slice;
-
-// 	$.kit = function( name_, base_, prototype_ ) {
-		
-// 		var fullName,
-// 			namespace = name_.split( '.' )[0],
-// 			constructor,
-// 			existingConstructor,
-// 			basePrototype;
-
-// 			name = name_.split( '.' )[1];
-// 			fullName = namespace + '-' + name;
-
-// 			if ( ! prototype_ ) {
-// 				prototype_ = base_;
-// 				base_ = $.Kit;
-// 			}
-
-// 			$.expr[ ":" ][ fullName.toLowerCase() ] = function( elem ) {
-// 				return !!$.data( elem, fullName );
-// 			};
-
-// 			$[ namespace ] = $[ namespace ] || {};
-// 			existingConstructor = $[ namespace ][ name ];
-// 			constructor = $[ namespace ][ name ] = function( options, element ) {
-// 				// allow instantiation without "new" keyword
-// 				if ( !this._createKit ) {
-// 					return new constructor( options, element );
-// 				}
-
-// 				// allow instantiation without initializing for simple inheritance
-// 				// must use "new" keyword (the code above always passes args)
-// 				if ( arguments.length ) {
-// 					this._createKit( options, element );
-// 				}
-// 			};
-// 			// extend with the existing constructor to carry over any static properties
-// 			$.extend( constructor, existingConstructor, {
-// 				version: prototype_.version,
-// 				// copy the object used to create the prototype in case we need to
-// 				// redefine the widget later
-// 				_proto: $.extend( {}, prototype_ ),
-// 				// track widgets that inherit from this widget in case this widget is
-// 				// redefined after a widget inherits from it
-// 				_childConstructors: []
-// 			});
-
-// 			basePrototype = new base_();
-
-// 			// we need to make the options hash a property directly on the new instance
-// 			// otherwise we'll modify the options hash on the prototype that we're
-// 			// inheriting from
-// 			basePrototype.options = $.kit.extend( {}, basePrototype.options );
-
-// 			$.each( prototype_, function( prop_, value_ ) {
-// 			if ( $.isFunction( value_ ) ) {
-// 				prototype_[ prop_ ] = (function() {
-// 					var _super = function() {
-// 							return base_.prototype[ prop_ ].apply( this, arguments );
-// 						},
-// 						_superApply = function( args ) {
-// 							return base_.prototype[ prop_ ].apply( this, args );
-// 						};
-						
-// 						return function() {
-// 							var __super = this._super,
-// 								__superApply = this._superApply,
-// 								returnValue;
-
-// 							this._super = _super;
-// 							this._superApply = _superApply;
-
-// 							returnValue = value_.apply( this, arguments );
-
-// 							this._super = __super;
-// 							this._superApply = __superApply;
-
-// 							return returnValue;
-// 						};
-// 					})();
-// 				}
-// 			});
-
-// 		constructor.prototype = $.kit.extend( basePrototype, {
-// 			// TODO: remove support for widgetEventPrefix
-// 			// always use the name + a colon as the prefix, e.g., draggable:start
-// 			// don't prefix for widgets that aren't DOM-based
-// 			widgetEventPrefix: basePrototype.widgetEventPrefix || name
-// 		}, prototype_, {
-// 			constructor: constructor,
-// 			namespace: namespace,
-// 			name: name,
-// 			fullName: fullName
-// 		});
-
-// 		// If this widget is being redefined then we need to find all widgets that
-// 		// are inheriting from it and redefine all of them so that they inherit from
-// 		// the new version of this widget. We're essentially trying to replace one
-// 		// level in the prototype chain.
-// 		if ( existingConstructor ) {
-// 			$.each( existingConstructor._childConstructors, function( i, child ) {
-// 				var childPrototype = child.prototype;
-
-// 				// redefine the child widget using the same prototype that was
-// 				// originally used, but inherit from the new version of the base
-// 				$.widget( childPrototype.namespace + "." + childPrototype.name, constructor, child._proto );
-// 			});
-// 			// remove the list of existing child constructors from the old constructor
-// 			// so the old child constructors can be garbage collected
-// 			delete existingConstructor._childConstructors;
-// 		} else {
-// 			base_._childConstructors.push( constructor );
-// 		}
-
-// 		console.log(name);
-// 		$.kit.bridge( name, constructor );
-// 	}
-
-// 	$.kit.extend = function( target_ ) {
-// 		var args = slice.call( arguments, 1 )
-// 		,	index = 0
-// 		,	length = args.length
-// 		,	key
-// 		,	value;
-
-// 		for( ; index < length; index++ ) {
-// 			for( key in args[ index ] ) {
-
-// 				value = args[ index ][ key ];
-				
-// 				if( args[ index ].hasOwnProperty( key ) && value !== undefined ) {
-
-// 					// If we have an object value in heritable args
-// 					// we need to propely extend it
-// 					if( $.isPlainObject( value ) ) {
-						
-// 						// If we already have a value in target_[ key ]
-// 						// then we need to extend it merge new and old values
-// 						// else just add new value
-// 						target_[ key ] = $.isPlainObject( target_[ key ] ) ?
-// 							$.kit.extend( {}, target_[ key ], value ) :
-// 							$.kit.extend( {}, value );
-
-// 					}
-// 					else
-// 					{
-// 						target_[ key ] = value;
-// 					}
-
-// 				}
-
-// 			}
-// 		}
-
-// 		return target_;
-// 	}
-
-// 	$.kit.bridge = function( name_, target_ ) {
-		
-// 		var fullName = target_.prototype.fullName;
-
-// 		$.fn[ name_ ] = function( options_ ) {
-
-// 			// If type of options is "string"
-// 			// then we should call method with passed name_
-// 			var isMethod = typeof options_ === 'string'
-// 			,	args = slice.call( arguments, 1 )
-// 			,	returnValue = this;
-
-// 			options = ! isMethod && args.length ?
-// 				$.kit.extend.apply( null, [options].concat( args ) ) :
-// 				options;
-
-// 			if( isMethod ) {
-// 				this.each(function() {
-// 					var instance = $.data(this, fullName)
-// 					,	methodValue;
-
-// 					if( ! instance ) {
-// 						return $.error( 'Can\'t call method' );
-// 					}
-
-// 					if( ! $.isFunction( instance[ options ] ) && options.charAt( 0 ) === '_' ) {
-// 						return $.error( 'No such method' );
-// 					}
-
-// 					methodValue = instance[ options ].apply( instance, args );
-					
-// 					if ( methodValue !== instance && methodValue !== undefined ) {
-// 						returnValue = methodValue && methodValue.jquery ?
-// 							returnValue.pushStack( methodValue.get() ) :
-// 							methodValue;
-						
-// 						return false;
-// 					}
-// 				})
-// 			}
-// 			else {
-// 				this.each(function() {
-// 					var instance = $.data( this, fullName );
-					
-// 					if ( instance ) {
-// 						instance.option( options || {} )._init(); //-----------------
-// 					}
-// 					else {
-// 						new object( options, this );
-// 					}
-// 				})
-// 			}
-
-// 			return returnValue;
-
-// 		}
-// 	}
-
-// 	$.Kit = function(  ) {};
-// 	$.Kit._childConstructors = [];
-// 	$.Kit.prototype = {
-// 		name: 'kit',
-// 		fullName: 'kit-fullname', //-----------
-// 		options: {
-// 			def: 'default'
-// 		},
-
-// 	}
-
-// })( jQuery, window, document );
--- js/plugins/maxmertkit.popup.js
+++ js/plugins/maxmertkit.popup.js
@@ -1,16 +1,290 @@
 ;(function ( $, window, document, undefined ) {
 
-	// $.kit('ms.popup', {
-	// 	options: {
-	// 		def: 'default'
-	// 	},
-	// 	open: function(){
-	// 		alert(1);
-	// 	}
-	// });
-	// 
-	$.popup = new $.kit('popup');
+	var _name = 'dropdown'
+	,	_defaults = {
+			placement: 'top'
+		,	offset: [0, 0]
+		,	autoOpen: false
+		,	template: '<div class="-dropdown" style="display:none"><i class="-arrow"></i><div class="-dropdown-content js-content"></div></div>'
+		}
+
+	$.dropdown = function( element_, options_ ) {
+		$.kit.apply( this, element_, options_ );
+
+		this.element = element_;
+		this.name = _name;
+
+		this.options = $.extend( {}, this.options, _defaults );
+		this.options = $.extend( {}, this.options, options_ );
+		
+		this.El = $(this.options.template);
+		$('body').append( this.El );
+		// CSS manupulations just in case
+		this.El.css({position: 'absolute'});
+		this.El.find('.-arrow').css({opacity: 0});
+
+		this._setOptions( this.options );
+
+		if( typeof $.dropdown.instanses === 'undefined' )
+			$.dropdown.instanses = []
+		
+		if( this.element !== undefined )
+			$.dropdown.instanses.push( this.element );
+		
+		this.timeout = null;
+
+		this.init();
+	}
+
+	$.dropdown.prototype = new $.kit();
+	$.dropdown.prototype.constructor = $.dropdown;
+
+	$.dropdown.prototype._setOption = function( key_, value_ ) {
+		var me  = this;
+		var $me = $(me.element);
+
+		switch( key_ ) {
+			case 'theme':
+				me.El.removeClass( '-' + me.options.theme + '-' );
+				me.El.addClass( '-' + value_ + '-' )
+			break;
+
+			case 'enabled':
+				value_ === true ? me.El.removeClass( '-disabled-' ) : me.El.addClass( '-disabled-' );
+			break;
+
+			case 'trigger':
+				var _events = value_.split(/[ ,]+/);
+					
+				if( me.options[key_].in !== undefined )
+					$me.off( 'mouseenter.' + me.name, 'click.' + me.name );
+
+				if( me.options[key_].out !== undefined )
+					$me.off( 'mouseleave.' + me.name, 'click.' + me.name );
+
+				me.options[key_] = {
+					in: _events[0] 
+				,   out: (_events[1] == undefined || _events[1] == '') ? _events[0] : _events[1]
+				};
+				
+				switch( me.options[key_].in ) {
+					case 'hover':
+						$me.on('mouseenter.' + me.name, function( event ) {
+							if( me.state == 'closed' )
+								me.open();
+						});
+					break;
+					
+					default:
+						$me.on( me.options[key_].in + '.' + me.name, function() {
+							if( me.state == 'closed' )
+								me.open();
+						});
+				}
+
+				switch( me.options[key_].out ) {
+					case 'hover':
+						$me.on('mouseleave.' + me.name, function( event ) {
+							me.close();
+						});
+					break;
+
+					default:
+						$me.on( me.options[key_].out + '.' + me.name, function() {
+							if( me.state == 'opened' )
+								me.close();
+						});
+				}
+			break;
+
+			case 'content':
+				if( value_ !== null )
+					me.El.find('.js-content').html(value_);
+				else
+					me.El.find('.js-content').html( $me.data('content') );
+			break;
+
+			case 'placement':
+				me.El.removeClass('_' + me.options.placement + '_')
+				me.El.addClass('_' + value_ + '_')
+			break;
+				
+		}
+
+		if( key_ !== 'trigger' )
+			me.options[ key_ ] = value_;
+	}
+
+	$.dropdown.prototype.init = function() {
+		var me  = this;
+
+		if( me.options.autoOpen )
+			me.open()
+	}
+
+	$.dropdown.prototype.open = function() {
+		var me  = this;
+		var $me = $(me.element);
+		
+		me.timeout = setTimeout(function(){
+			if( me.options.enabled === true && me.state !== 'opened' ) {
+				
+				me.state = 'in';
+
+				if( me.options.beforeOpen !== undefined && (typeof me.options.beforeOpen === 'object' || typeof me.options.beforeOpen === 'function' )) {
+					
+					try {
+						me.options.beforeOpen()
+							.done(function(){
+								me._open();
+							})
+							.fail(function(){
+								me.state = 'closed';
+								$me.trigger('ifNotOpened.' + me.name);
+								$me.trigger('ifOpenedOrNot.' + me.name);
+							})
+					} catch( e ) {
+						me._open();
+					}
+					
+				}
+				else {
+					me._open();
+				}
+			}
+		}, me.options.delay)
+	}
+
+	$.dropdown.prototype._open = function() {
+		var me  = this;
+		var $me = $(me.element);
+
+		if( me.state === 'in' ) {
+			
+			if( me.options.onlyOne )
+				
+				$.each( me._getOtherInstanses( $.dropdown.instanses ), function() {
+					if( $.data( this, 'kit-' + me.name ).getState() === 'opened' )
+						$.data( this, 'kit-' + me.name ).close();
+				});
+
+			me._setPosition();
+			
+			if( me.options.animation !== null && me.options.animation !== false )
+			{	
+				me.El.slideDown({
+					duration: me.options.animationDuration,
+					easing: me.options.animation,
+					complete: function(){
+						me.El.find('.-arrow').animate({opacity: 1});
+					}
+				});
+			}
+			else
+			{
+				me.El.find('.-arrow').css({opacity: 1});
+				me.El.show();
+			}
+			
+			me.state = 'opened';
+			$me.trigger('open.' + me.name);
+		}
+
+		$me.trigger('ifOpenedOrNot.' + me.name);
+	}
+
+	$.dropdown.prototype.close = function() {
+		var me  = this;
+		var $me = $(me.element);
+		
+		clearTimeout( me.timeout );
+
+		if( me.options.enabled === true && me.state !== 'closed' ) {
+
+			me.state = 'out';
+
+			if( me.options.beforeClose != undefined && (typeof me.options.beforeClose === 'object' || typeof me.options.beforeClose === 'function' ))
+			{
+				
+				try {
+					me.options.beforeClose()
+						.done(function(){
+							me._close();
+						})
+						.fail(function(){
+							$me.trigger('ifNotClosed.' + me.name);
+							$me.trigger('ifClosedOrNot.' + me.name);
+							me.state = 'opened';
+						})
+				} catch( e ) {
+					me._close();
+				}
+				
+			}
+			else {
+				me._close();
+			}
+		}
+	}
+
+	$.dropdown.prototype._close = function() {
+		var me  = this;
+		var $me = $(me.element);
+
+		if( me.state === 'out' ) {
+			me.El.find('.-arrow').css({opacity: 0});
+			me.El.hide();
+			me.state = 'closed';
+
+			$me.trigger('close');	
+		}
+		
+		$me.trigger('ifClosedOrNot.' + me.name);
+	}
+
+	$.dropdown.prototype._setPosition = function() {
+		var me  = this;
+		var $me = $(me.element);
+
+		var actualWidth = $me.width();
+		var actualHeight = $me.height();
+		var actualPosition = $me.offset();
+		
+		var pos = {}
+
+		switch( me.options.placement ) {
+			case 'top':
+				pos = { top: actualPosition.top - me.El.height() - 10 + me.options.offset[0], left: actualPosition.left + actualWidth / 2 - me.El.width() / 2 + me.options.offset[1] }
+			break;
+
+			case 'bottom':
+				pos = { top: actualPosition.top + actualHeight + 5 + me.options.offset[0], left: actualPosition.left + actualWidth / 2 - me.El.width() / 2 + me.options.offset[1] };
+			break;
+
+			case 'left':
+				pos = { top: actualPosition.top + actualHeight / 2 - me.El.height(), left: actualPosition.left - me.El.width() - 15 + me.options.offset[1] }
+			break;
+
+			case 'right':
+				pos = { top: actualPosition.top + actualHeight / 2 - me.El.height(), left: actualPosition.left + actualWidth + me.options.offset[1] + 5 }
+			break;
+		}
+
+		me.El.css(pos);
+	}
+
+	$.fn[_name] = function( options_ ) {
+		return this.each(function() {
+			if( ! $.data( this, 'kit-' + _name ) ) {
+				$.data( this, 'kit-' + _name, new $.dropdown( this, options_ ) );
+			}
+			else {
+				typeof options_ === 'object' ? $.data( this, 'kit-' + _name )._setOptions( options_ ) :
+					typeof options_ === 'string' && options_.charAt(0) !== '_' ? $.data( this, 'kit-' + _name )[ options_ ] : $.error( 'What do you want to do?' );
+			}
+		});
+	}
 	
-	console.log( $.popup );
+	
 
 })( jQuery, window, document );
